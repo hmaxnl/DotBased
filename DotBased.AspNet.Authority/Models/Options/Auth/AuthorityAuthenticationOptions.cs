e22b7790dde10299efa7418af00ffac5e459df49
max <max.holleman@hotmail.nl>
2025-04-28 17:11:46 +0200

    [WIP] Reworking authentication service

--- DotBased.AspNet.Authority/Models/Options/Auth/AuthorityAuthenticationOptions.cs
+++ DotBased.AspNet.Authority/Models/Options/Auth/AuthorityAuthenticationOptions.cs
@@ -5,6 +5,11 @@ public class AuthorityAuthenticationOptions
     public AuthenticationSecurityOptions Security { get; set; } = new AuthenticationSecurityOptions();
     public SessionOptions Session { get; set; } = new SessionOptions();
     public string DefaultScheme { get; set; } = string.Empty;
+    public string DefaultAuthenticateScheme { get; set; } = string.Empty;
+    public string DefaultChallengeScheme { get; set; } = string.Empty;
+    public string DefaultForbidScheme { get; set; } = string.Empty;
+    public string DefaultSignInScheme { get; set; } = string.Empty;
+    public string DefaultSignOutScheme { get; set; } = string.Empty;
     public List<SchemeInfo> SchemeMap { get; set; } = [];
 }
 
